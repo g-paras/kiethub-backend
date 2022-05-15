812f1b9509287976ed19fbcd256c38baf24a7b2d
Paras Gupta <guptaparas039@gmail.com>
2022-05-16 00:34:22 +0530

    alter columns and add new fields in profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,22 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 course_choices = ['B.Tech', 'MCA', 'M.Tech', 'MBA']
-course_choices = [(str(i), j) for i, j in enumerate(course_choices)]
+course_list = [(str(i), j) for i, j in enumerate(course_choices)]
+
+branch_choice = ['ECE', 'EEE', 'IT', 'CO', 'CSE', 'CSIT', 'ME', 'CE']
+branch_list = [(str(i), j) for i, j in enumerate(branch_choice)]
 
 # Create your models here.
 class Profile(models.Model):
     # name = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     dob = models.DateField(null=True)
-    course = models.CharField(max_length=10, choices=course_choices, default='B.Tech')
+    course = models.CharField(max_length=10, choices=course_list, default='B.Tech')
     specialization = models.CharField(max_length=50, null=True)
     enrolled_year = models.DateField(null=True)
     current_year = models.IntegerField(null=True)
-    contact = models.IntegerField(null=True)
+    contact = models.CharField(max_length=10, null=True)
+    section = models.CharField(max_length=1, null=True)
     github = models.CharField(max_length=100, null=True)
     linkedin = models.CharField(max_length=100, null=True)
     profile = models.ImageField(upload_to="profile", null=True, blank=True)
     # library_id = models.CharField(max_length=10, null=True)
 
     def __str__(self):
-        return f"Profile<user={self.user}, ...>"
+        return f"{self.user}, {course_choices[int(self.course)]}, {self.specialization}, {self.current_year}"
