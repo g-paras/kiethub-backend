f5575ad5a28c0352769e158dadf38bd2deace4ac
Paras Gupta <guptaparas039@gmail.com>
2022-05-15 22:46:56 +0530

    update settings

--- kiethub_backend/settings.py
+++ kiethub_backend/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -84,7 +85,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    }, 
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'detid8u2rkl7v9', 
+    #     'USER': 'kfzueolwcvkqqk', 
+    #     'PASSWORD': '1c566cec5ad3ad050d0ae3eb8960442304b7dd4f793060e7b2a3b6c866505bc6',
+    #     'HOST': 'ec2-34-230-110-100.compute-1.amazonaws.com', 
+    #     'PORT': '5432',
+    # }
 }
 
 
@@ -123,6 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'kiethub_backend' / 'static',
+)
+
+MEDIA_ROOT= BASE_DIR / 'media/'
+MEDIA_URL= "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
