49af412efd4d906f091610fe214fa4936078cde9
Prerna_25 <khushipandey2511@gmail.com>
2022-05-19 12:32:49 +0530

    ..

--- account/serializers.py
+++ account/serializers.py
@@ -6,12 +6,14 @@ from rest_framework.serializers import ModelSerializer
 class RegisterSerializer(ModelSerializer):
     class Meta:
         model = User
+        #fields = ('username','email','password','confirmpassword')
         fields = ('username','password','first_name', 'last_name')
         extra_kwargs = {
             'password':{'write_only': True},
         }
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'],     password = validated_data['password']  ,first_name=validated_data['first_name'],  last_name=validated_data['last_name'])
+        user=User.objects.create_user(username=validated_data['username'],password=validated_data['password'],first_name=validated_data['first_name'],last_name=validated_data['last_name'])
+        #user = User.objects.create_user( username=validated_data['username'],email = validated_data['email'] , password = validated_data['password'],confirmpassword = validated_data['confirmpassword'] )
         return user
 
 # User serializer
--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-course_choices = ['B.Tech', 'MCA', 'M.Tech', 'MBA']
+course_choices = ["B.Tech", "MCA", "M.Tech", "MBA"]
 course_choices = [(str(i), j) for i, j in enumerate(course_choices)]
 
 # Create your models here.
@@ -9,7 +9,7 @@ class Profile(models.Model):
     # name = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     dob = models.DateField(null=True)
-    course = models.CharField(max_length=10, choices=course_choices, default='B.Tech')
+    course = models.CharField(max_length=10, choices=course_choices, default="B.Tech")
     specialization = models.CharField(max_length=50, null=True)
     enrolled_year = models.DateField(null=True)
     current_year = models.IntegerField(null=True)
