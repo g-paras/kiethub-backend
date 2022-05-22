604d9934816d4e6c6a8d26029f1a8ae5dae38845
Paras Gupta <guptaparas039@gmail.com>
2022-05-22 23:08:08 +0530

    add tags field to post model

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="posts", blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=255, null=True)
     body = models.TextField()
 
     def __str__(self) -> str:
