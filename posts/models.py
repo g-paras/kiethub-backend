02cc6fb382ef867b4f9c40183f04a7d00283ed42
Paras Gupta <guptaparas039@gmail.com>
2022-05-23 06:11:00 +0530

    add field created_at & updated_at, use profile as author

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from profiles.models import Profile
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="posts", blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     tags = models.CharField(max_length=255, null=True)
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.title} | {self.author}"
