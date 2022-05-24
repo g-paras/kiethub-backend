50a25b9d79538ecd9cfb8f192866351447073040
Paras Gupta <guptaparas039@gmail.com>
2022-05-24 11:22:25 +0530

    add readonly fields: username and profile_image in Post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 
 from .models import Post
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    username = serializers.ReadOnlyField(source='author.user.username')
+    profile_image = serializers.ImageField(source='author.profile_image', read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
