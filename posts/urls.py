26e0ecbe4a73b5f4c23b3eab09d4d1566845874b
Paras Gupta <guptaparas039@gmail.com>
2022-06-03 19:37:16 +0530

    add route for user post list view

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PostView
+from .views import PostView, UserPostView
 
 router = routers.DefaultRouter()
 router.register('', PostView)
 
 urlpatterns = [
-    # path('', get_profile),
     path('', include(router.urls)),
-    
+    path('user/<int:pk>/', UserPostView.as_view(), name='get-user-post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.generics import ListAPIView
 
 from posts.models import Post
 from posts.serializers import PostSerializer
-# from posts.pagination import CustomPageNumberPagination
-# Create your views here.
-from rest_framework.pagination import PageNumberPagination
 from .paginations import MyPageNumberPagination
 
 class PostView(viewsets.ModelViewSet):
@@ -13,3 +11,14 @@ class PostView(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     pagination_class= MyPageNumberPagination
     permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class UserPostView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    model = serializer_class.Meta.model
+
+    def get_queryset(self):
+        user_id = self.kwargs['pk']
+        queryset = self.model.objects.filter(author=user_id)
+        return queryset
