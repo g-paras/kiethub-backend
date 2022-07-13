922698e742a6d28a1bf890f4d3f83cc95cd3b303
Paras Gupta <guptaparas039@gmail.com>
2022-07-13 15:02:41 +0000

    add field in serializer pass profile serializer in user serializer as read_only field pass post serializer in profile serializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,23 +1,28 @@
 from django.contrib.auth.models import User
-
+# from posts.serializers import PostSerializer
+from profiles.serializers import ProfileSerializer
 from rest_framework.serializers import ModelSerializer
 
 # Register serializer
 class RegisterSerializer(ModelSerializer):
     class Meta:
         model = User
-        #fields = ('username','email','password','confirmpassword')
-        fields = ('username','password','first_name', 'last_name')
+        # fields = ('username','email','password','confirmpassword')
+        fields = ("username", "password", "first_name", "last_name")
         extra_kwargs = {
-            'password':{'write_only': True},
+            "password": {"write_only": True},
         }
+
     def create(self, validated_data):
-        user=User.objects.create_user(username=validated_data['username'],password=validated_data['password'],first_name=validated_data['first_name'],last_name=validated_data['last_name'])
-        #user = User.objects.create_user( username=validated_data['username'],email = validated_data['email'] , password = validated_data['password'],confirmpassword = validated_data['confirmpassword'] )
+        user = User.objects.create_user(username=validated_data["username"], password=validated_data["password"], first_name=validated_data["first_name"], last_name=validated_data["last_name"])  # type: ignore
+        # user = User.objects.create_user( username=validated_data['username'],email = validated_data['email'] , password = validated_data['password'],confirmpassword = validated_data['confirmpassword'] )
         return user
+
 
 # User serializer
 class UserSerializer(ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,8 +1,10 @@
 from .models import Profile
+from posts.serializers import PostSerializer
 from rest_framework import serializers
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    posts = PostSerializer(many=True, read_only=True)
     username = serializers.ReadOnlyField(source='user.username')
     email = serializers.ReadOnlyField(source='user.email')
     class Meta:
