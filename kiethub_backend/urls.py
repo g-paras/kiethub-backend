9e075e5a76a86bb8e7a0e2346b36a4fafa3625bb
Paras Gupta <guptaparas039@gmail.com>
2022-07-13 15:04:08 +0000

    add route for verify token

--- kiethub_backend/urls.py
+++ kiethub_backend/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -40,7 +40,8 @@ urlpatterns = [
     path('post/', include('posts.urls')),
     path('account/', include('account.urls')),
     path('profile/', include('profiles.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
