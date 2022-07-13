f8a643eae3df463fae7fa215e94a8e5918db03af
Paras Gupta <guptaparas039@gmail.com>
2022-07-13 15:03:38 +0000

    Create UserRetrieve api

--- account/api.py
+++ account/api.py
@@ -3,14 +3,25 @@ from rest_framework.response import Response
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
 
-#Register API
+# Register API
 class RegisterApi(generics.GenericAPIView):
     serializer_class = RegisterSerializer
-    def post(self, request, *args,  **kwargs):
+
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-            "user": UserSerializer(user,    context=self.get_serializer_context()).data,
-            "message": "User Created Successfully.  Now perform Login to get your token",
-        })
+        return Response(
+            {
+                "user": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "message": "User Created Successfully.  Now perform Login to get your token",
+            }
+        )
+
+
+class UserRetrieveApi(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    lookup_field = "username"
+    queryset = User.objects.all()
