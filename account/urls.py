df4aa8bd03c422fcead19e3b114d5547668468a2
Paras Gupta <guptaparas039@gmail.com>
2022-07-13 15:04:39 +0000

    create route for user retrieve api

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 import django.conf.urls 
-from django.urls import path, include
-from .api import RegisterApi
+from django.urls import path
+from .api import RegisterApi, UserRetrieveApi
 
 urlpatterns = [
     path('api/register', RegisterApi.as_view()),
+    path('user/<str:username>/', UserRetrieveApi.as_view())
 ]
