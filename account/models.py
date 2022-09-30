af798540e12830f6768f06c06daf82246f9db80c
Paras Gupta <guptaparas039@gmail.com>
2022-10-01 02:38:48 +0530

    create custom user model closes #1

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class AuthModel(models.Model):
@@ -9,3 +11,15 @@ class AuthModel(models.Model):
     is_verified = models.BooleanField("is_verified", default=False)
     auth_token = models.UUIDField("auth_token", null=True)
     # created_at = models.DateTimeField("created_at", auto_now=True)
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+    is_verified = models.BooleanField(
+        _("verified"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as verified. "
+        ),
+    )
